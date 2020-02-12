f8684c0b3f1400855c65888defe47febf3c879a6
alex <abarlow90@gmail.com>
2020-02-12 22:52:31 +0000

    added more funnels with some cool colour palletes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import GeometricSnake from './GeometricSnake.js';
 
 function App() {
 	const array1 = [
-		{ skewAmount: 10, fill: '#384b60', label: 'one' },
-		{ skewAmount: 22, fill: '#5c93c4', label: 'two' },
-		{ skewAmount: 34, fill: '#bedafa', label: 'three' },
-		{ skewAmount: 40, fill: '#f8f7f2', label: 'four' },
-		{ skewAmount: 50, fill: '#a83e6c', label: 'five' }
+		{ skewAmount: 10, fill: '#384b60' },
+		{ skewAmount: 22, fill: '#5c93c4' },
+		{ skewAmount: 34, fill: '#bedafa' },
+		{ skewAmount: 40, fill: '#f8f7f2' },
+		{ skewAmount: 48, fill: '#a83e6c' }
 	];
 	const array2 = [
 		{ skewAmount: 10, fill: '#384b60', label: 'one' },
@@ -20,19 +20,34 @@ function App() {
 
 	return (
 		<div className="App">
+			<button onClick={array => setSwitcheroo(array.length === 3 ? array1 : array2)}>Do the old switcheroo</button>
 			<header className="App-header">
-				<button onClick={array => setSwitcheroo(array.length === 3 ? array1 : array2)}>
-					Do the old switcheroo
-				</button>
 				<GeometricSnake elements={switcheroo} />
 				<GeometricSnake
 					showDescriptionLine={true}
 					elements={[
-						{ skewAmount: 10, fill: '#384b60' },
-						{ skewAmount: 22, fill: '#5c93c4' },
-						{ skewAmount: 34, fill: '#bedafa' },
-						{ skewAmount: 40, fill: '#f8f7f2' },
-						{ skewAmount: 50, fill: '#a83e6c' }
+						{ skewAmount: 20, fill: '#324448', label: 'one' },
+						{ skewAmount: 22, fill: '#6b8b8d', label: 'two' },
+						{ skewAmount: 34, fill: '#b6cdb1', label: 'three' },
+						{ skewAmount: 40, fill: '#f87c56', label: 'four' }
+					]}
+				/>
+				<GeometricSnake
+					elements={[
+						{ skewAmount: 10, fill: '#3f5c6d' },
+						{ skewAmount: 40, fill: '#b9efa3' },
+						{ skewAmount: 43, fill: '#ffa2cb' },
+						{ skewAmount: 48, fill: '#eff9f7' }
+					]}
+				/>
+				<GeometricSnake
+					viewBoxWidth={300}
+					elementHeight={75}
+					elements={[
+						{ skewAmount: 5, fill: '#99dedf' },
+						{ skewAmount: 20, fill: '#e45865' },
+						{ skewAmount: 20, fill: '#fcd06b' },
+						{ skewAmount: 37, fill: '#f7f6ee' }
 					]}
 				/>
 				<br />
