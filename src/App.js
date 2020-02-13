7c649b16e877dfb124571038dc86214872eed9d8
alex <abarlow90@gmail.com>
2020-02-13 01:31:00 +0000

    implimented random skew widths

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.css';
-import GeometricSnake from './GeometricSnake.js';
+import GeometricSnake from './GeometricSnake';
+import useSwitcheroo from './useSwitcheroo';
+import beRandom from './beRandom';
+import Button from './Button';
 
 function App() {
-	const array1 = [
-		{ skewAmount: 10, fill: '#384b60' },
-		{ skewAmount: 22, fill: '#5c93c4' },
-		{ skewAmount: 34, fill: '#bedafa' },
-		{ skewAmount: 40, fill: '#f8f7f2' },
-		{ skewAmount: 48, fill: '#a83e6c' }
-	];
-	const array2 = [
-		{ skewAmount: 10, fill: '#384b60', label: 'one' },
-		{ skewAmount: 20, fill: '#5c93c4', label: 'two' },
-		{ skewAmount: 30, fill: '#bedafa', label: 'three' }
-	];
+	const coolColours = useMemo(
+		() => [
+			'#324448',
+			'#6b8b8d',
+			'#b6cdb1',
+			'#f87c56',
+			'#3f5c6d',
+			'#b9efa3',
+			'#ffa2cb',
+			'#eff9f7',
+			'#99dedf',
+			'#e45865',
+			'#fcd06b',
+			'#f7f6ee'
+		],
+		[]
+	);
+	const [triggerSwitcheroo, theOldSwitcheroo] = useSwitcheroo(coolColours);
 
-	const [switcheroo, setSwitcheroo] = useState(array1);
+	const [triggerAnotherSwitcheroo, whatsThisTwoSwitcheroos] = useSwitcheroo(coolColours);
+
+	const [currentLyric, setCurrentLyric] = useState(0);
+
+	const bennyBennassi = ['Push me', 'And then just touch me', 'Till I can get my', 'Satisfaction'];
+
+	const [buttonColours, setButtonColours] = useState({ backgroundColour: 'fff', textColour: '000' });
+
+	function triggerSomeSwitcheroos() {
+		triggerSwitcheroo();
+		triggerAnotherSwitcheroo();
+		setCurrentLyric(lastLyric => (bennyBennassi.length > lastLyric + 1 ? lastLyric + 1 : 0));
+		setButtonColours(() => {
+			let backgroundColourIndex = beRandom(0, coolColours.length);
+			let textColourIndex = beRandom(0, coolColours.length);
+
+			if (backgroundColourIndex === textColourIndex) {
+				textColourIndex = beRandom(0, coolColours.length);
+			}
+
+			return { backgroundColour: coolColours[backgroundColourIndex], textColour: coolColours[textColourIndex] };
+		});
+	}
 
 	return (
 		<div className="App">
-			<button onClick={array => setSwitcheroo(array.length === 3 ? array1 : array2)}>Do the old switcheroo</button>
 			<header className="App-header">
-				<GeometricSnake elements={switcheroo} />
+				<GeometricSnake elements={theOldSwitcheroo} />
 				<GeometricSnake
 					showDescriptionLine={true}
 					elements={[
@@ -32,7 +62,12 @@ function App() {
 						{ skewAmount: 40, fill: '#f87c56', label: 'four' }
 					]}
 				/>
+				<Button colours={buttonColours} onClick={triggerSomeSwitcheroos}>
+					{bennyBennassi[currentLyric]}
+				</Button>
 				<GeometricSnake
+					viewBoxWidth={300}
+					elementHeight={100}
 					elements={[
 						{ skewAmount: 10, fill: '#3f5c6d' },
 						{ skewAmount: 40, fill: '#b9efa3' },
@@ -40,16 +75,7 @@ function App() {
 						{ skewAmount: 48, fill: '#eff9f7' }
 					]}
 				/>
-				<GeometricSnake
-					viewBoxWidth={300}
-					elementHeight={75}
-					elements={[
-						{ skewAmount: 5, fill: '#99dedf' },
-						{ skewAmount: 20, fill: '#e45865' },
-						{ skewAmount: 20, fill: '#fcd06b' },
-						{ skewAmount: 37, fill: '#f7f6ee' }
-					]}
-				/>
+				<GeometricSnake elements={whatsThisTwoSwitcheroos} />
 				<br />
 			</header>
 		</div>
